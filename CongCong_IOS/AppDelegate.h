a01ab2b693fb9cbd72a3f2a596e1936f9b639b88
Santisco <1160302773@qq.com>
2016-05-13 20:22:14 +0800

    开启葱葱

---
+++ CongCong_IOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CongCong_IOS
+//
+//  Created by 吴冠龙 on 16/5/13.
+//  Copyright © 2016年 吴冠龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CongCong_IOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CongCong_IOS
+//
+//  Created by 吴冠龙 on 16/5/13.
+//  Copyright © 2016年 吴冠龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
